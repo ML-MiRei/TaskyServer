fac063f00dcd1e8e287c164cb44f5c26d1ad1d8e
Jie <evgeniagaigerova@yandex.ru>
2025-02-13 13:21:31 +0300

    add Email Service

---
+++ Microservices/AuthorizationService/AuthorizationService.Applicaion/Abstractions/Services/IConfirmationService.cs
@@ -0,0 +1,7 @@
+﻿namespace AuthenticationService.Applicaion.Abstractions.Services
+{
+    public interface IConfirmationService
+    {
+        public Task ConfirmationEmail(string email);
+    }
+}
---
+++ Microservices/AuthorizationService/AuthorizationService.Infrustructure/Implementations/Services/ConfirmationService.cs
@@ -0,0 +1,17 @@
+﻿using AuthenticationService.Applicaion.Abstractions.Services;
+using System.Configuration;
+
+namespace AuthenticationService.Infrastructure.Implementations.Services
+{
+    class ConfirmationService : IConfirmationService
+    {
+
+        public Task ConfirmationEmail(string email)
+        {
+            var senderEmail = ConfigurationManager.AppSettings["app_email"];
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
